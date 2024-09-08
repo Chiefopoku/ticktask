6270680ec011ddf019c86d27f3e5f38b1a80c6ff
Chiefopoku <kwabenaopokujnr@gmail.com>
2024-09-08 13:02:32 +0000

    .env added

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -12,13 +12,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqC7V1TPl-bayOwd5Xi3wlWrr1GlFs9mQ",
-  authDomain: "ticktask-944d7.firebaseapp.com",
-  projectId: "ticktask-944d7",
-  storageBucket: "ticktask-944d7.appspot.com",
-  messagingSenderId: "504566858572",
-  appId: "1:504566858572:web:d769b0541c8c872f9e05e9",
-  measurementId: "G-NWGWQHP6B0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
